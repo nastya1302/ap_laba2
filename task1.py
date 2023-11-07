5524a79ab6d2a841b3b4078e313a60295019292d
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-07 21:14:23 +0400

    task2

--- task1.py
+++ task1.py
@@ -1,35 +1,36 @@
 import csv
 import os
 
-def writing_absolute_path(name: str) -> list:
-    abs_path = os.path.join(os.path.abspath('dataset1'), name)
+def writing_absolute_path(name: str, dir: str) -> list:
+    abs_path = os.path.join(os.path.abspath(dir), name)
     list_images = os.listdir(abs_path)
     rez_paths = []
     for i in list_images:
        rez_paths.append(os.path.join(abs_path, i))
     return rez_paths
 
-def writing_relative_path(name: str) -> list:
-    rel_path = os.path.join(os.path.relpath('dataset1'), name)
+def writing_relative_path(name: str, dir: str) -> list:
+    rel_path = os.path.join(os.path.relpath(dir), name)
     list_images = os.listdir(rel_path)
     rez_paths = []
     for i in list_images:
        rez_paths.append(os.path.join(rel_path, i)) 
     return rez_paths
 
-def creating_csvfile(name: str, names: str) -> None:
+def creating_csvfile(name: str, names: str, dir: str) -> None:
     with open(name + ".csv", 'w', newline='') as f:
         filewriter = csv.writer(f, delimiter=',', lineterminator='\r')
         filewriter.writerow(['Absolute path', 'Relative path', 'Class name'])
         for i in names:
-            abs_paths = writing_absolute_path(i)
-            rel_paths = writing_relative_path(i)
+            abs_paths = writing_absolute_path(i, dir)
+            rel_paths = writing_relative_path(i, dir)
             for abs_path,rel_path in zip(abs_paths, rel_paths):
                 filewriter.writerow([abs_path, rel_path, i])
 
 def main() -> None:
+    dir = "dataset1"
     names = ("rose", "tulip")
-    creating_csvfile("Annotasion1", names)
+    creating_csvfile("Annotasion1", names, dir)
 
 if __name__ == "__main__":
     main()
--- task2.py
+++ task2.py
@@ -1,8 +1,49 @@
 import os
+import shutil
+import csv
+
+def copy_images(old_dir: str, new_dir: str, name: str) -> None:
+    path = os.path.join(os.path.abspath(old_dir), name)
+    list_images = os.listdir(path)
+    for img in list_images:
+        shutil.copy(
+        os.path.join(path, img),
+        os.path.join(new_dir, f'{name}_{img}')
+        )
+
+def creating_csvfile(name: str, dir: str):
+    with open(name + ".csv", 'w', newline='') as f:
+        filewriter = csv.writer(f, delimiter=',', lineterminator='\r')
+        filewriter.writerow(['Absolute path', 'Relative path'])
+        abs_paths = writing_absolute_path(dir)
+        rel_paths = writing_relative_path(dir)
+        for abs_path,rel_path in zip(abs_paths, rel_paths):
+            filewriter.writerow([abs_path, rel_path])
+
+def writing_absolute_path(dir: str):
+    abs_path = os.path.join(os.path.abspath(dir))
+    list_images = os.listdir(abs_path)
+    rez_paths = []
+    for i in list_images:
+       rez_paths.append(os.path.join(abs_path, i))
+    return rez_paths
+    
+def writing_relative_path(dir):
+    rel_path = os.path.join(os.path.relpath(dir))
+    list_images = os.listdir(rel_path)
+    rez_paths = []
+    for i in list_images:
+       rez_paths.append(os.path.join(rel_path, i)) 
+    return rez_paths
 
 def main() -> None:
-    if not os.path.isdir('dataset2'):
-        os.mkdir('dataset2')
+    new_dir = "dataset2"
+    if not os.path.isdir(new_dir):
+        os.mkdir(new_dir)
+    names = ("rose", "tulip")
+    for name in names:
+        copy_images("dataset1", "dataset2", name)
+    creating_csvfile("Annotasion2", new_dir)
 
 if __name__ == "__main__":
     main()
