423630ef220a63fab65df1f4955c9dfa203df744
Nastya Mytareva <nastya.mytareva.04@gmail.com>
2023-11-18 22:30:36 +0400

    adjustment for pip8

--- task1.py
+++ task1.py
@@ -10,11 +10,11 @@ def writing_absolute_path(name: str, dir: str) -> List[str]:
     and a list of the names of the elements in it. Adds absolute paths
     of elements to the list. Returns a list of paths.
     """
-    abs_path:str = os.path.join(os.path.abspath(dir), name)
-    list_images:List[str] = os.listdir(abs_path)
-    rez_paths:List[str] = []
+    abs_path: str = os.path.join(os.path.abspath(dir), name)
+    list_images: List[str] = os.listdir(abs_path)
+    rez_paths: List[str] = []
     for i in list_images:
-       rez_paths.append(os.path.join(abs_path, i))
+        rez_paths.append(os.path.join(abs_path, i))
     return rez_paths
 
 
@@ -25,40 +25,40 @@ def writing_relative_path(name: str, dir: str) -> List[str]:
     and a list of the names of the elements in it. Adds relative paths
     of elements to the list. Returns a list of paths.
     """
-    rel_path:str = os.path.join(os.path.relpath(dir), name)
-    list_images:List[str] = os.listdir(rel_path)
-    rez_paths:List[str] = []
+    rel_path: str = os.path.join(os.path.relpath(dir), name)
+    list_images: List[str] = os.listdir(rel_path)
+    rez_paths: List[str] = []
     for i in list_images:
-       rez_paths.append(os.path.join(rel_path, i)) 
+        rez_paths.append(os.path.join(rel_path, i))
     return rez_paths
 
 
 def creating_csvfile(namecsv: str) -> None:
     """
-    The function takes as input the name for the .csv file, 
+    The function takes as input the name for the .csv file,
     creates a .csv file with the passed name and writes the column headers.
     """
-    with open(namecsv + ".csv", 'w', newline='') as f:
-        filewriter = csv.writer(f, delimiter=',', lineterminator='\r')
-        filewriter.writerow(['Absolute path', 'Relative path', 'Class name'])
+    with open(namecsv + ".csv", "w", newline="") as f:
+        filewriter = csv.writer(f, delimiter=",", lineterminator="\r")
+        filewriter.writerow(["Absolute path", "Relative path", "Class name"])
 
 
 def write_path(namecsv: str, name: str, dir: str):
     """
     The function opens a .csv file and writes absolute and relative paths to the desired columns.
     """
-    with open(namecsv + ".csv", 'a', newline='') as f:
-        filewriter = csv.writer(f, delimiter=",", lineterminator='\r')
-        abs_paths:List[str] = writing_absolute_path(name, dir)
-        rel_paths:List[str] = writing_relative_path(name, dir)
-        for abs_path,rel_path in zip(abs_paths, rel_paths):
+    with open(namecsv + ".csv", "a", newline="") as f:
+        filewriter = csv.writer(f, delimiter=",", lineterminator="\r")
+        abs_paths: List[str] = writing_absolute_path(name, dir)
+        rel_paths: List[str] = writing_relative_path(name, dir)
+        for abs_path, rel_path in zip(abs_paths, rel_paths):
             filewriter.writerow([abs_path, rel_path, name])
 
 
 def main(name1: str, name2: str, dir: str) -> None:
     """
-    The main() function is supplied with the names of classes 
-    and the name of the directory. A .csv file with the name "Annotation" 
+    The main() function is supplied with the names of classes
+    and the name of the directory. A .csv file with the name "Annotation"
     is created and absolute and relative paths are written there alternately from each class.
     """
     creating_csvfile("Annotasion1")
@@ -67,4 +67,4 @@ def main(name1: str, name2: str, dir: str) -> None:
 
 
 if __name__ == "__main__":
-    main('rose', 'tulip', 'dataset1')
+    main("rose", "tulip", "dataset1")
--- task4.py
+++ task4.py
@@ -4,11 +4,11 @@ from typing import List
 
 def get_next(name_class: str) -> str:
     """
-    The function gets the name of the class and returns 
+    The function gets the name of the class and returns
     the relative path to the next image, when the images end returns None.
     """
-    path:str = os.path.join('dataset1', name_class)
-    name_list:List[str] = os.listdir(path)
+    path: str = os.path.join("dataset1", name_class)
+    name_list: List[str] = os.listdir(path)
     name_list.append(None)
     for i in range(len(name_list)):
         if name_list[i] is not None:
@@ -19,11 +19,11 @@ def get_next(name_class: str) -> str:
 
 def main(name: str) -> None:
     """
-    The function main() receives the class name as input and passes it to the function, 
+    The function main() receives the class name as input and passes it to the function,
     which returns the relative path to the next image, and prints them.
     """
     print(*get_next(name))
 
 
 if __name__ == "__main__":
-    main('rose')
+    main("rose")
